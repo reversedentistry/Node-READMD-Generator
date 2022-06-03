116b592a7f288f8a53e987f41ddd630e9c169334
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-03 15:33:42 -0700

    Added conditional statement to render license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,17 +2,17 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "Apache") {
-    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`; 
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
   } else if (license === "MIT") {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`; 
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
   } else if (license === "ISC") {
-    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`; 
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
   } else if (license === "GNU") {
-    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`; 
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
   } else if (license === "Mozilla") {
-    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`; 
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
   } else if (license === "Boost") {
-    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`; 
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
   } else {
     return ``;
   }
@@ -22,36 +22,42 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "Apache") {
-    return `This project is [Apache](https://choosealicense.com/licenses/apache-2.0/) licensed.`; 
+    return `This project is [Apache](https://choosealicense.com/licenses/apache-2.0/) licensed.`;
   } else if (license === "MIT") {
-    return `This project is [MIT](https://choosealicense.com/licenses/mit/) licensed.`; 
+    return `This project is [MIT](https://choosealicense.com/licenses/mit/) licensed.`;
   } else if (license === "ISC") {
-    return `This project is [ISC](https://choosealicense.com/licenses/mit/) licensed.`; 
+    return `This project is [ISC](https://choosealicense.com/licenses/mit/) licensed.`;
   } else if (license === "GNU") {
-    return `This project is [GNU](https://choosealicense.com/licenses/gpl-3.0/) licensed.`; 
+    return `This project is [GNU](https://choosealicense.com/licenses/gpl-3.0/) licensed.`;
   } else if (license === "Mozilla") {
-    return `This project is [GNU](https://choosealicense.com/licenses/gpl-3.0/) licensed.`; 
+    return `This project is [GNU](https://choosealicense.com/licenses/gpl-3.0/) licensed.`;
   } else if (license === "Boost") {
     return `This project is [Boost](https://choosealicense.com/licenses/bsl-1.0/) licensed.`;
   } else {
-    return ``; 
+    return ``;
   }
-    
-}    
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `### Licenses 
-  ${renderLicenseBadge(license)}
-  ${renderLicenseLink(license)}`
-}
+  if (license === "None") {
+    return ``
+  } else {
+    return `### Licenses 
+    ${renderLicenseBadge(license)}
+    ${renderLicenseLink(license)}` 
+  }}; 
+ 
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${ data.title }
 
-`;
+      `;
 }
 
 module.exports = generateMarkdown;
