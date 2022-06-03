4ad12b9cdbe757eb5a6c62f7d34203b7ef347484
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-02 23:14:19 -0700

    Added array of questions

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer"); 
 const fs = require("fs"); 
-const generateMarkdown = require("./utils/generateMarkdown.js");
+const generate = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input", 
+        name: "title", 
+        message: "Enter your project title."  
+    }, 
+    {
+        type: "input", 
+        name: "desc", 
+        message: "Enter a brief description of your project."
+    }, 
+    {
+        type: "input", 
+        name: "installation",
+        message: "Enter any installation instructions."
+
+    },
+    {
+        type: "input", 
+        name: "usage", 
+        message: "Enter any usage instructions."
+    },    
+    {
+        type: "input", 
+        name: "contribution",
+        message: "Enter any contribution guidelines." 
+    },
+    {
+        type: "input", 
+        name: "tests",
+        message: "Enter any tests you'd like to include." 
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
