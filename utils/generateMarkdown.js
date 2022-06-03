dd898f531ec7331fc5d2063ff1bad06b2f1df3a8
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-03 13:34:38 -0700

    Added function to choose license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`; 
+  } else if (license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`; 
+  } else if (license === "ISC") {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`; 
+  } else if (license === "GNU") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`; 
+  } else if (license === "Mozilla") {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`; 
+  } else {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`; 
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
