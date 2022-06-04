08908b27a074d85f47d865b38153eaeda3610a56
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-04 09:53:15 -0700

    Fixed some syntax so license content would show

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === "Apache") {
+  if (license == "Apache") {
     return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-  } else if (license === "MIT") {
+  } else if (license == "MIT") {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-  } else if (license === "ISC") {
+  } else if (license == "ISC") {
     return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
-  } else if (license === "GNU") {
+  } else if (license == "GNU") {
     return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-  } else if (license === "Mozilla") {
+  } else if (license == "Mozilla") {
     return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
-  } else if (license === "Boost") {
+  } else if (license == "Boost") {
     return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
   } else {
-    return ``;
+    return "";
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === "Apache") {
+  if (license == "Apache") {
     return `This project is [Apache](https://choosealicense.com/licenses/apache-2.0/) licensed.`;
-  } else if (license === "MIT") {
+  } else if (license == "MIT") {
     return `This project is [MIT](https://choosealicense.com/licenses/mit/) licensed.`;
-  } else if (license === "ISC") {
+  } else if (license == "ISC") {
     return `This project is [ISC](https://choosealicense.com/licenses/mit/) licensed.`;
-  } else if (license === "GNU") {
+  } else if (license == "GNU") {
     return `This project is [GNU](https://choosealicense.com/licenses/gpl-3.0/) licensed.`;
-  } else if (license === "Mozilla") {
+  } else if (license == "Mozilla") {
     return `This project is [GNU](https://choosealicense.com/licenses/gpl-3.0/) licensed.`;
-  } else if (license === "Boost") {
+  } else if (license == "Boost") {
     return `This project is [Boost](https://choosealicense.com/licenses/bsl-1.0/) licensed.`;
   } else {
-    return ``;
+    return "";
   }
 
 }
@@ -42,7 +42,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === "None") {
+  if (license == "None") {
     return `This project is unlicensed.`;
   } else {
     return `${renderLicenseBadge(license)}
@@ -58,6 +58,7 @@ function generateMarkdown(data) {
     ${renderLicenseBadge(data.license)}
 
     ## Description
+
     ${data.desc}
 
     ## Table of Contents
@@ -68,18 +69,23 @@ function generateMarkdown(data) {
     - [License](#license)
 
     ## Installation
+
     ${data.installation}
 
     ## Usage
+
     ${data.usage}
 
     ## Contributions
+
     ${data.contribution}
 
     ## Tests
+
     ${data.tests}
 
     ## License 
+
     ${renderLicenseSection(data.license)}
 
     ## Questions
