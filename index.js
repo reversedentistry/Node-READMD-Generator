8c76963e02d511f1650a2cec3466655c4571e543
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-03 23:14:25 -0700

    Added validate functions to question array so some prompts require responses

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "Enter your project title."
+        message: "Enter your project title.",
+        validate: titleInput  => {
+            if (titleInput) {
+                return true; 
+            } else {
+                console.log("Project title is required. Please enter a title."); 
+                return false; 
+            }
+        }
     },
     {
         type: "input",
         name: "desc",
-        message: "Enter a brief description of your project."
+        message: "Enter a brief description of your project.",
+        validate: descInput => {
+            if (descInput) {
+                return true; 
+            } else {
+                console.log("Project description is required. Please enter a description.");
+                return false; 
+            }
+        }
     },
     {
         type: "input",
@@ -41,22 +57,51 @@ const questions = [
         name: "license",
         message: "Please specify a license used.",
         choices: ["Apache", "MIT", "ISC", "GNU", "Mozilla", "Boost", "None"],
+        validate: licenseChoice => {
+            if (licenseChoice.length = 1) {
+                return true;
+            } else {
+                console.log("You must choose a license option.");
+                return false;
+            }
+        }
     },
     {
         type: "input", 
         name: "username", 
-        message: "Please enter your GitHub username."
-    }, 
+        message: "Please enter your GitHub username.",
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true; 
+            } else {
+                console.log("Username is required.");
+                return false; 
+            }
+    }}, 
     {
         type: "input", 
         name: "link", 
-        message: "Please enter the link to your GitHub profile."
-    }, 
+        message: "Please enter the link to your GitHub profile.",
+        validate: linkInput => {
+            if (linkInput) {
+                return true; 
+            } else {
+                console.log("GitHub link is required.");
+                return false; 
+            }
+    }}, 
     {
         type: "input", 
         name: "email", 
-        message: "Please enter your email."
-    }, 
+        message: "Please enter your email.",
+        validate: emailInput => {
+            if (emailInput) {
+                return true; 
+            } else {
+                console.log("Email is required.");
+                return false; 
+            }
+    }}, 
 ];
 
 // TODO: Create a function to write README file
